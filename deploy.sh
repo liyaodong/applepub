92f7352a9c91acd037a6091ba8388568530da99a
Gavin <gavin.li1986@gmail.com>
2015-03-08 10:59:42 +0800

    用 rsync 代替 scp

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,5 @@
 #! /usr/bin/env bash
 
-ssh geekpark.net 'rm -rf /data/www/gpk_zt/apple_event/**'
-scp -r deploy/** geekpark.net:/data/www/gpk_zt/apple_event/
+rsync -av deploy/ geekpark.net:/data/www/gpk_zt/apple_event/
 
 echo '发布完成'
